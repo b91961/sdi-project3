7a43eae7ebf8096a0ca27fbb258bae45a190aec8
Jamal Moubarak <jamalmoubarak@Jamals-MacBook-Pro.local>
2013-02-21 16:22:41 -0500

    Added json to html file, added all of my json variables and added a command to pull json data in java file.

--- Moubarak_Jamal_assignment3.js
+++ Moubarak_Jamal_assignment3.js
@@ -3,3 +3,12 @@
 // Jamal Moubarak
 // Project 3
 // SDI 1302
+
+var handleData = function (json) {
+	for (var i = 0; i < json.clients.length; i++){
+		var client = json.clients[i];
+		console.log("Phone Number: " + client.number + ", Name: " + client.name + ", Type of System: " + client.system + ", Missed Calls Today: " + client.missedCalls);
+	};
+};
+
+handleData(json2);
--- sdi-JSON.js
+++ sdi-JSON.js
@@ -1,3 +1,89 @@
 // Jamal Moubarak
 // Project 2
 // SDI 1302
+
+var json = {
+	"client": {
+		"4198061514": {
+			"name": "Tony",
+			"system": "DVR",
+			"missedCalls": 8
+		},
+		"4194567890": {
+			"name": "Cleo",
+			"system": "POS",
+			"missedCalls": 2
+		},
+		"5677890123": {
+			"name": "Fidel",
+			"system": "DVR",
+			"missedCalls": 1	
+		},
+		"4194558877": {
+			"name": "Benny",
+			"system": "POS",
+			"missedCalls": 5
+		},
+		"5676439489": {
+			"name": "Steve",
+			"system": "DVR",
+			"missedCalls": 4
+		}
+	}
+
+};
+
+for (var key in json.clients) {
+	var client = json.clients[key];
+	//client.name
+};
+
+
+
+var json2 = {
+	"clients": [
+		{
+			"number": "4198061514",
+			"name": "Tony",
+			"system": "DVR",
+			"missedCalls": 8
+		},
+		{
+			"number": "4194567890",
+			"name": "Cleo",
+			"system": "POS",
+			"missedCalls": 2
+		},
+		{
+			"number": "5677890123",
+			"name": "Fidel",
+			"system": "DVR",
+			"missedCalls": 1
+		},
+		{
+			"number": "4194558877",
+			"name": "Benny",
+			"system": "POS",
+			"missedCalls": 5
+		},
+		{
+			"number": "5676439489",
+			"name": "Steve",
+			"system": "DVR",
+			"missedCalls": 4
+		}
+	]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
