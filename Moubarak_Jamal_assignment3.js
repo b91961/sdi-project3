452cda2e51e0d2f0f9238a1960a662df9b8b1cd3
Jamal Moubarak <jamalmoubarak@Jamals-MacBook-Pro.local>
2013-02-21 17:17:03 -0500

    Added intro to my story and Global Variables.

--- Moubarak_Jamal_assignment3.js
+++ Moubarak_Jamal_assignment3.js
@@ -4,6 +4,16 @@
 // Project 3
 // SDI 1302
 
+// Intro to Story	
+console.log("It's 9am... My alarm clock is annoyingly chirping, " + 
+			"so I open my eyes, reach over and grab my phone to find " + 
+			"missed calls from 5 of my clients " + "Here is a list of the clients " + 
+			"with their customer info and how many times they called:");
+
+// Global variables
+var system = ["DVR", "POS", "PC"],
+	clients = ["Tony", "Cleo", "Fidel", "Benny", "Steve"];
+
 var handleData = function (json) {
 	for (var i = 0; i < json.clients.length; i++){
 		var client = json.clients[i];
@@ -12,3 +22,11 @@ var handleData = function (json) {
 };
 
 handleData(json2);
+
+
+
+var jsonstring = JSON.stringify(json);
+// console.log(jsonstring);
+
+var receivedjson = JSON.parse(jsonstring);
+// console.log(receivedjson);
